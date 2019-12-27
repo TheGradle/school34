eb98f0660c2060c77a903850c34ea00bc04d9b2d
TheGradle <33810054+TheGradle@users.noreply.github.com>
2019-12-27 19:09:39 +0200

    fixs & about_html

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 1
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 300000000
   });
 });
