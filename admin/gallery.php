a654bad556a09ba16ccb533e6b3ad7b26e7216a6
theGradle <adamchuk.v.1@gmail.com>
2020-01-09 20:42:35 +0200

    php 4.2

---
+++ admin/gallery.php
@@ -0,0 +1,113 @@
+<?php 
+  require_once "../includes/config.php";
+
+  $errors = [];
+
+  $caption = htmlentities(mysqli_real_escape_string($connection, $_POST['caption']));
+  $subtitle = htmlentities(mysqli_real_escape_string($connection, $_POST['subtitle']));
+  $excerpt = htmlentities(mysqli_real_escape_string($connection, $_POST['excerpt']));
+
+  if(isset($_POST['submit'])){
+    if(trim($caption) == '') {
+      $errors[] = 'Введіть назву новини!';
+    }
+
+    if(trim($subtitle) == '') {
+      $errors[] = 'Опишіть коротку про новину!';
+    }
+
+    if(trim($excerpt) == '') {
+      $errors[] = 'Напишіть текст ноивини!';
+    }
+
+    if(empty($errors)) {
+      $request = mysqli_query($connection, "INSERT INTO `news` (`caption`, `subtitle`, `excerpt`) VALUES ('$caption', '$subtitle', '$excerpt')");
+    } else {
+      echo array_shift($errors);
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Галерея - Адмін панель - Миколаївський заклад загальної середньої освіти №34</title>
+  <meta name="description" content="Загальноосвітня школа № 34 - це другий дім для учнів та працівників школи. Ми завжди раді всім хто хоче, буде, або вже навчається в нашій школі.">
+  <meta name="keywords" content="мета-теги, шаблон, html, css">
+  <meta name="robots" content="index,follow,noodp">
+  <meta name="googlebot" content="index,follow">
+  <meta name="google" content="nositelinkssearchbox">
+  <link rel="stylesheet" href="../css/reset.min.css">
+  <link rel="stylesheet" href="../css/main.min.css">
+  <link rel="stylesheet" href="../css/admin.min.css">
+  <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+  <meta name="google" content="notranslate"><!-- Подтверждает авторство страницы в Google Search Console -->
+  <meta name="robots" content="noindex">
+  <meta name="googlebot" content="noindex">
+
+  <link rel="shortcut icon" href="../img/icons/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="../img/icons/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="../img/icons/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="../img/icons/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../img/icons/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="../img/icons/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="../img/icons/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="../img/icons/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="../img/icons/apple-touch-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="../img/icons/apple-touch-icon-180x180.png" />
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-TileImage" content="../img/icons/mstile-144x144.png">
+  <meta name="theme-color" content="#fd333b">
+  <meta name="msapplication-navbutton-color" content="#fff">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#fd333b">
+</head>
+<body>
+  <div class="page">
+    <h2 class="page__title">Галерея - Адмін панель</h2>
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Новини</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="reports.php">Звіти</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="zno.php">ЗНО</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="gallery.php">Галерея</a>
+      </li>
+    </ul>
+    <div class="admin">
+      <h3>Додати фото</h3>
+      <form action="" method="POST">
+        <div class="form-group">
+          <label for="img">Завантажте зображення</label>
+          <input type="file" class="form-control-file" id="img">
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg" name="submit">Відправити</button>
+      </form>
+    </div>
+    <div class="admin">
+      <h3>Видалити фото</h3>
+      <form action="" method="POST">
+        <div class="form-group">
+          <label for="id">ID фото, якого треба видалити</label>
+          <input type="text" class="form-control" id="id" name="id">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-primary btn-lg" name="submit">Видалити</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  <script src="//code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="../js/main.min.js"></script>
+</body>
+</html>
--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,15 @@
 <?php
 
-$config = [
-  'server' => 'localhost',
-  'username' => 'mysql',
-  'password' => 'mysql',
-  'name' => 'school34'
-];
+define('HOST', 'localhost');
+define('USER', 'mysql');
+define('PASS', 'mysql');
+define('DB', 'geek_brains_shop');
+
+define('SITE_ROOT', __DIR__ . "/../");
+define('WWW_ROOT', SITE_ROOT . 'public/');
+define('DATA_DIR', SITE_ROOT . 'data/');
+define('TPL_DIR', SITE_ROOT . 'templates/');
+define('ENG_DIR', SITE_ROOT . 'engine/');
+define('IMG_DIR', 'img/');
 
 require 'db.php';
