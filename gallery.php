3e6f0c24f80f7f39ec7cbff7f1330ea17bc27178
theGradle <adamchuk.v.1@gmail.com>
2020-03-13 22:32:58 +0200

    new gallery

--- gallery.php
+++ gallery.php
@@ -7,9 +7,9 @@
   <meta charset="utf-8">
   <title>Галерея - Миколаївський заклад загальної середньої освіти №34</title>
   <meta name="description" content="Загальноосвітня школа № 34 - це другий дім для учнів та працівників школи. Ми завжди раді всім хто хоче, буде, або вже навчається в нашій школі.">
-  <link rel="stylesheet" href="../css/reset.min.css">
-  <link rel="stylesheet" href="../css/main.min.css">
-  <link rel="stylesheet" href="../css/gallery.min.css">
+  <link rel="stylesheet" href="css/reset.min.css">
+  <link rel="stylesheet" href="css/main.min.css">
+  <link rel="stylesheet" href="css/gallery.min.css">
   <link rel="stylesheet" href="css/animate.min.css">
   <?php
     require_once "templates/head.php";
@@ -23,29 +23,54 @@
     <div class="wrap">
       <h2 class="page__title wow fadeInUp animation">Галерея</h2>
       <div class="gallery">
-        <div class="gallery-block">
+        <div class="gallery-block wow fadeInUp animation">
           <h3 class="gallery-block-date">20 жовтня 2019</h3>
           <div class="gallery-block-img">
-            <img src="https://avatars0.githubusercontent.com/u/33810054?s=460&v=4" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" class="gallery-block-img__item">
+            <div class="gallery-block-img__item">
+              <img src="https://avatars0.githubusercontent.com/u/33810054?s=460&v=4" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" id="image">
+            </div>
           </div>
         </div>
-        <div class="gallery-block">
+        <div class="gallery-block wow fadeInUp animation">
           <h3 class="gallery-block-date">20 жовтня 2019</h3>
           <div class="gallery-block-img">
-            <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/nwbfa-_zodJpe1JewtbrCY0bicxitJcx2YlTOiPr1BJJ0aJ48n7_JSnzlyyLFQ0YbUkWh32l6o8NuHX39sgNVERr_g_-WF8xCyXr0e-nzMkf8_Pmi204aWM3ZgXIcut7F-wBx8DjvsQKlQKmFHurR_nAdStRPhOHLgpVBUWlh1APjuZCLBmK47IaAM4BXAl_QI6gxq4KzFDfkJhNL6Dl3OnaRtppF1v1xy_pJQrqOGvcz_i2aBrszQalouBdLIpX6UEu1O3f3jnt1Pm8ItZMncFFf4ao_8uisagJaoflVEc-eUUvTMXaOMUPN59kY9JQDSMbXeNfbMd9bQK1j28gDCNy5gNn_9aJ5RMdmkfu2JWkM-_ik_0ThlXjcZk8QRezzKHD-q3i6M2wiVYg6wFUSangTmfn86rvssgKshdLyn4Gdgt7WcLyN3lBuoAIzuIfy-4c87D4YauIaBlfEYsdjfMKrHeuYs2OmERgWdYFe-SrJP_-2jEHzLzc9ZlcCnXifEdlwQhha_xcTQYPc3Rfl9KPCOtU5Big5meN40KzLjJffhN6TKwtYm_39zEQLV-suiOyFnqiLiurAX2KGBncFlUQoAwDFWmosh4tBwUH7Sdamn3JXYZXYHqyIzpoVZe8sPR9Nnvi0edHNMIhiGmGtHSQlrl1u1rBAev3gs-InMUwXk4WyXSgnXY=w568-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/nwbfa-_zodJpe1JewtbrCY0bicxitJcx2YlTOiPr1BJJ0aJ48n7_JSnzlyyLFQ0YbUkWh32l6o8NuHX39sgNVERr_g_-WF8xCyXr0e-nzMkf8_Pmi204aWM3ZgXIcut7F-wBx8DjvsQKlQKmFHurR_nAdStRPhOHLgpVBUWlh1APjuZCLBmK47IaAM4BXAl_QI6gxq4KzFDfkJhNL6Dl3OnaRtppF1v1xy_pJQrqOGvcz_i2aBrszQalouBdLIpX6UEu1O3f3jnt1Pm8ItZMncFFf4ao_8uisagJaoflVEc-eUUvTMXaOMUPN59kY9JQDSMbXeNfbMd9bQK1j28gDCNy5gNn_9aJ5RMdmkfu2JWkM-_ik_0ThlXjcZk8QRezzKHD-q3i6M2wiVYg6wFUSangTmfn86rvssgKshdLyn4Gdgt7WcLyN3lBuoAIzuIfy-4c87D4YauIaBlfEYsdjfMKrHeuYs2OmERgWdYFe-SrJP_-2jEHzLzc9ZlcCnXifEdlwQhha_xcTQYPc3Rfl9KPCOtU5Big5meN40KzLjJffhN6TKwtYm_39zEQLV-suiOyFnqiLiurAX2KGBncFlUQoAwDFWmosh4tBwUH7Sdamn3JXYZXYHqyIzpoVZe8sPR9Nnvi0edHNMIhiGmGtHSQlrl1u1rBAev3gs-InMUwXk4WyXSgnXY=w568-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" class="gallery-block-img__item">
-            <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" class="gallery-block-img__item">
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/nwbfa-_zodJpe1JewtbrCY0bicxitJcx2YlTOiPr1BJJ0aJ48n7_JSnzlyyLFQ0YbUkWh32l6o8NuHX39sgNVERr_g_-WF8xCyXr0e-nzMkf8_Pmi204aWM3ZgXIcut7F-wBx8DjvsQKlQKmFHurR_nAdStRPhOHLgpVBUWlh1APjuZCLBmK47IaAM4BXAl_QI6gxq4KzFDfkJhNL6Dl3OnaRtppF1v1xy_pJQrqOGvcz_i2aBrszQalouBdLIpX6UEu1O3f3jnt1Pm8ItZMncFFf4ao_8uisagJaoflVEc-eUUvTMXaOMUPN59kY9JQDSMbXeNfbMd9bQK1j28gDCNy5gNn_9aJ5RMdmkfu2JWkM-_ik_0ThlXjcZk8QRezzKHD-q3i6M2wiVYg6wFUSangTmfn86rvssgKshdLyn4Gdgt7WcLyN3lBuoAIzuIfy-4c87D4YauIaBlfEYsdjfMKrHeuYs2OmERgWdYFe-SrJP_-2jEHzLzc9ZlcCnXifEdlwQhha_xcTQYPc3Rfl9KPCOtU5Big5meN40KzLjJffhN6TKwtYm_39zEQLV-suiOyFnqiLiurAX2KGBncFlUQoAwDFWmosh4tBwUH7Sdamn3JXYZXYHqyIzpoVZe8sPR9Nnvi0edHNMIhiGmGtHSQlrl1u1rBAev3gs-InMUwXk4WyXSgnXY=w568-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/nwbfa-_zodJpe1JewtbrCY0bicxitJcx2YlTOiPr1BJJ0aJ48n7_JSnzlyyLFQ0YbUkWh32l6o8NuHX39sgNVERr_g_-WF8xCyXr0e-nzMkf8_Pmi204aWM3ZgXIcut7F-wBx8DjvsQKlQKmFHurR_nAdStRPhOHLgpVBUWlh1APjuZCLBmK47IaAM4BXAl_QI6gxq4KzFDfkJhNL6Dl3OnaRtppF1v1xy_pJQrqOGvcz_i2aBrszQalouBdLIpX6UEu1O3f3jnt1Pm8ItZMncFFf4ao_8uisagJaoflVEc-eUUvTMXaOMUPN59kY9JQDSMbXeNfbMd9bQK1j28gDCNy5gNn_9aJ5RMdmkfu2JWkM-_ik_0ThlXjcZk8QRezzKHD-q3i6M2wiVYg6wFUSangTmfn86rvssgKshdLyn4Gdgt7WcLyN3lBuoAIzuIfy-4c87D4YauIaBlfEYsdjfMKrHeuYs2OmERgWdYFe-SrJP_-2jEHzLzc9ZlcCnXifEdlwQhha_xcTQYPc3Rfl9KPCOtU5Big5meN40KzLjJffhN6TKwtYm_39zEQLV-suiOyFnqiLiurAX2KGBncFlUQoAwDFWmosh4tBwUH7Sdamn3JXYZXYHqyIzpoVZe8sPR9Nnvi0edHNMIhiGmGtHSQlrl1u1rBAev3gs-InMUwXk4WyXSgnXY=w568-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/qeDQ0z-7ztl3ZX3LP5rdOraNrbbje-CQB5Lh5BCPJYr14SPzcWYL4t8UCyCQwrj6odsaln1nYamySB_k3B7S3OumhDCG0ZEup0hT4ArFHpAmqb_SCf5-m8rZGE3sQkYAbQeKhF9811tEq6gJvxnjR7cscOCrEP1HFjyg8XRTe3U-221k6BgZ10blUtsIEC4Dcw154XjaD9C0tAFN24UEsiepct1R2e-_CldqYOEkQd-lOjHXOYQHkjCfdrGYan6USOolK2TUCZF0Jg88OWgDyZ8-ZWC3WojQWXvs1FG7-sepW7jr2IJzOqk0y1Uj_vMGVFo0FbuN_yONF4t6OgoeoPAN7mMsVY5MnJer_6YOcja8LJsJU36BHfhZyHrlCBhoDKazfyJIwonEmGKQmmjMQkGoOKEFPAnxKfSBnDutq3OLMVl2-ttOAPaF3cLBtTzxu6fzJzvB5gao4vUaIOSpyHzWDDz68jzjNgWjv8bOv2ta_ytk1ELyfXMzGLF-g9Y1Unm9uR9YkYHW0L28ofRoqcRICeUuThB5_2pSrApJZAiK090TaJhdwZgtOglvm-FPUWMQiFR4FKUehtnbPdXvTvsUifl4268uxQ_EHybhNcbWn-_wzjJvaxqCk0pUZla0ubdIcuKUTJgNJ3TSQI7Zwzkaooe8ZHyIr9CI8B-jVZni0j0Mvpo1Pbw=w1346-h757-no" alt="" id="image">
+            </div>
+            <div class="gallery-block-img__item">
+              <img src="https://lh3.googleusercontent.com/BE06UzhDZlXLYUBQnWursNWHnJAjlwko1qusTh427gHJ8YK-UdD_2qUftGpNlgJ8ppok690XTz4EZLX5XMU-hZs80KhzvX5p_2capmPFCpRkpOtv21cqX5XcXgHrjqGLWo7sWQTk0gQ3kD9gQtLZkLQSZV4DOBldmdKatBy2nYctBTe8IulhXz8R5KGW2F1LNeLHcExZTDYCaWJWyxvWqxMMbdteXeiFIg0JESNuWLLD2dv2AnJfDnE_NrMUBKe4LXaODmAUiNgZbco8t8mBXXtXJIn_tSMTdx_c0mAPq4fTdnWy9wyV0eNjHzoI0CbnwjNomE-DcZiUfkc-RlW1XTVoQxehPNlGcge7SXAWQcVRasLMoa_t260bw-h1S4mYwjI32SXPpPCJk5yscjClcYKCJwucCg2X-EZ6WuQ7Rd_9aKczdkA-GkgR-Tw6syNNpki4U5k0qpGPV8S6B9VK0-H9yT9Du373qxy27PyHTYqcnZFSPyxQ1pGPJfZW7Ze-r-0DSzIw1P6KsmJk_xEnEY8L-YE30W7E477CZJq0YpLZhPhZOj5ghDi5ED20UfVVQdnSNx2ycXkMjjyMm3st5ymxstz06MzumVtp3cjoMieawCVJuVjWCohICGFt6GpyKma084lw-pjMbbO4LjkiNmRx_9lHlDFoaE0N7zV9Wsg0tjgzgiKPN7Y=w1346-h757-no" alt="" id="image">
+            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  <div class="block-preview">
+    <div class="block-preview-photo"><img src="" alt="" class="block-preview-photo__img"></div>
   </div>
   <div class="footer">
     <?php
@@ -57,6 +82,22 @@
   <script src="js/wow.min.js"></script>
   <script>
     new WOW().init();
+
+    $('.gallery-block-img__item').click(function(e) {
+      $('.block-preview-photo__img').attr("src", "");
+
+      var src = e.target.src;
+      
+      $('.block-preview-photo__img').attr("src", src);
+
+      $('.block-preview').fadeIn();
+    });
+
+    $('.block-preview-photo').click(function(e) {
+      if(this === e.target) {
+        $('.block-preview').fadeOut();
+      }
+    });
   </script>
   <script src="https://kit.fontawesome.com/4589ffe11e.js" crossorigin="anonymous"></script>
 </body>
